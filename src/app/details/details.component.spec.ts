6737504be2ccedd369914b0885b0df07c43fdd84
Nelson Ameyo <nelson@sodium.co.ke>
2022-11-18 00:50:27 +0300

    Adding tests for details page component

--- src/app/details/details.component.spec.ts
+++ src/app/details/details.component.spec.ts
@@ -1,23 +1,25 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { DetailsComponent } from './details.component';
 
 describe('DetailsComponent', () => {
-  let component: DetailsComponent;
-  let fixture: ComponentFixture<DetailsComponent>;
+    let component: DetailsComponent;
+    let fixture: ComponentFixture<DetailsComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ DetailsComponent ]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [DetailsComponent],
+            imports: [HttpClientTestingModule, RouterTestingModule],
+        }).compileComponents();
 
-    fixture = TestBed.createComponent(DetailsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(DetailsComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create create details page component', () => {
+        expect(component).toBeTruthy();
+    });
 });
